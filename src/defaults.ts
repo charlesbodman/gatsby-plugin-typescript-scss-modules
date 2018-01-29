6799aa5fd5d4d30b65cb5f571c2beff412291498
Chad Sheets <cjsheets@gmail.com>
2018-01-28 17:03:07 -0800

    remove file name from hash

--- src/defaults.ts
+++ src/defaults.ts
@@ -2,5 +2,5 @@ exports.defaults = {
   camelCase: true,
   namedExport: true,
   modules: true,
-  localIdentName: "[name]__[local]___[hash:base64:5]",
+  localIdentName: "[local]__[hash:base64:5]",
 };
