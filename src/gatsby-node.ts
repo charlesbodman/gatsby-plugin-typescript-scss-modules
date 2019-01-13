f5809e41b671d8675b9e9fc0f54ed2b23d48059c
Charlie Bodman <charlie.bodman@kanoapps.com>
2019-01-12 22:09:45 -0800

    Updated plugin to gatsby 2

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -1,84 +1,40 @@
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const { cssModulesConfig } = require(`gatsby-1-config-css-modules`)
-const { defaults } = require("./defaults");
 
-const postCssLoader = "postcss-loader";
-const cssLoader = "typings-for-css-modules-loader";
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
 
-exports.modifyWebpackConfig = ({ config, stage }, options) => {
+    const sassFiles = /\.s[ac]ss$/;
+    const sassModulesFiles = /\.module\.s[ac]ss$/;
 
-  const sassFiles = /\.s[ac]ss$/
-  const sassModulesFiles = /\.module\.s[ac]ss$/
-  const sassLoader = `sass?${JSON.stringify(options)}`
+    const cssTypescriptTypings = {
+        loader: 'typings-for-css-modules-loader',
+        options: {
+            modules: true,
+            localIdentName: '[name]__[local]--[hash:base64:5]',
+            namedExport: true,
+            camelCase: true,
+            url: true
+        }
+    };
 
-  config.removeLoader("cssModules");
+    const sassLoader = {
+        loader: `sass-loader`
+    };
 
-  const configuration = JSON.stringify({
-    ...defaults,
-    ...options,
-  });
-  const loader = `${cssLoader}?${configuration}`;
-
-  switch (stage) {
-    case "develop": {
-      config.loader("cssModules", {
+    const cssModulesLoader = {
         test: sassFiles,
         exclude: sassModulesFiles,
-        loaders: ["style-loader", loader, postCssLoader, sassLoader],
-      });
+        loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader', use: [
+                cssTypescriptTypings,
+                loaders.postcss(stage),
+                sassLoader
+            ]
+        })
+    };
 
-      config.loader(`sassModules`, {
-        test: sassModulesFiles,
-        loaders: [`style`, cssModulesConfig(stage), `postcss`, sassLoader],
-      })
-      return config;
-    }
-    case "build-css": {
-      config.loader("cssModules", {
-        test: sassFiles,
-        exclude: sassModulesFiles,
-        loader: ExtractTextPlugin.extract("style-loader", [
-          loader,
-          postCssLoader,
-          sassLoader,
-        ]),
-      });
-
-      config.loader(`sassModules`, {
-        test: sassModulesFiles,
-        loader: ExtractTextPlugin.extract(`style`, [
-          cssModulesConfig(stage),
-          `postcss`,
-          sassLoader,
-        ]),
-      })
-
-      return config;
-    }
-    case "develop-html":
-    case "build-html":
-    case "build-javascript": {
-      config.loader("cssModules", {
-        test: sassFiles,
-        exclude: sassModulesFiles,
-        loader: ExtractTextPlugin.extract("style-loader", [
-          loader,
-          postCssLoader,
-          sassLoader,
-        ]),
-      });
-
-      config.loader(`sassModules`, {
-        test: sassModulesFiles,
-        loader: ExtractTextPlugin.extract(`style`, [
-          cssModulesConfig(stage),
-          sassLoader,
-        ]),
-      })
-      return config;
-    }
-    default: {
-      return config;
-    }
-  }
+    actions.setWebpackConfig({
+        module: {
+            rules: [cssModulesLoader]
+        }
+    });
 };
